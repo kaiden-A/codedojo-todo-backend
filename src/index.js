46a31c6f9a2db83672837b0379738f69845a0165
Kaiden-A <amirikhwanfaisal@gmail.com>
2026-05-29 11:33:07 +0800

    add morgan

--- src/index.js
+++ src/index.js
@@ -4,14 +4,16 @@ const express = require('express');
 const cors = require('cors');
 const taskRoutes = require('./routes/tasks');
 const sql = require('./config/db');
+const morgan = require('morgan');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(morgan('dev'))
 app.use(cors());
 app.use(express.json());
 
-app.use('/tasks', taskRoutes);
+app.use('/api/tasks', taskRoutes);
 
 app.get('/', (req, res) => {
   res.json({ message: 'To-Do API is running' });
